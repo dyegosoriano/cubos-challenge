d54fff4d00fa3db4de5e72d004e3144b94156d57
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-09-07 04:30:47 -0300

    feat: implementing login screen integration

--- frontend/src/components/Input/InputError.tsx
+++ frontend/src/components/Input/InputError.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 type IProps = React.ComponentProps<'span'> & { error_message?: string }
 
-export const InputError: React.FC<IProps> = ({ error_message, ...props }) => (
-  <span className="row-start-3 h-4 text-xs text-purple-9" {...props}>
-    {error_message}
-  </span>
+export const InputError: React.FC<IProps> = ({ error_message }) => (
+  <span className="row-start-3 h-4 text-xs text-purple-9">{error_message}</span>
 )
--- frontend/src/pages/public/Login.tsx
+++ frontend/src/pages/public/Login.tsx
@@ -1,32 +1,56 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { Container } from '../../components/Container'
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
+import { useAuth } from '../../hooks/useAuth'
+
+const validation = z.object({
+  password: z.string().max(16, 'Senha deve ter no máximo 16 caracteres').min(8, 'Senha deve ter no mínimo 8 caracteres'),
+  email: z.string().min(6, 'Email deve ter no mínimo 6 caracteres').email('Email inválido')
+})
+
+type FormData = z.infer<typeof validation>
 
 export const Login = () => {
+  const { handleSignIn } = useAuth()
+
+  const { handleSubmit, register, formState } = useForm<FormData>({ resolver: zodResolver(validation) })
+  const { errors, isSubmitting } = formState
+
+  async function onSubmit(data: FormData) {
+    await handleSignIn(data)
+  }
+
   return (
     <div className="flex items-center justify-center h-full">
       <Container>
-        <Input.Container>
-          <Input.Label text="E-mail" />
-          <Input.Field placeholder="Digite seu email" type="email" />
-        </Input.Container>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input.Container>
+            <Input.Label text="E-mail" />
+            <Input.Field placeholder="Digite seu email" type="email" {...register('email')} />
+            {errors.email && <Input.Error error_message={errors.email.message} />}
+          </Input.Container>
 
-        <Input.Container>
-          <Input.Label text="Senha" />
-          <Input.Field placeholder="Digite sua senha" type="password" />
-        </Input.Container>
+          <Input.Container>
+            <Input.Label text="Senha" />
+            <Input.Field placeholder="Digite sua senha" type="password" {...register('password')} />
+            {errors.password && <Input.Error error_message={errors.password.message} />}
+          </Input.Container>
 
-        <div className="flex justify-between">
-          <Link className="text-purple-9 underline" to="create-account">
-            Criar conta
-          </Link>
+          <div className="flex justify-between">
+            <Link className="text-purple-9 underline" to="create-account">
+              Criar conta
+            </Link>
 
-          <Link to="/home">
-            <Button>Entrar</Button>
-          </Link>
-        </div>
+            <Button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? 'Entrando...' : 'Entrar'}
+            </Button>
+          </div>
+        </form>
       </Container>
     </div>
   )
